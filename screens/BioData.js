92966d3c62132a4436595e87f3af2788b2c75bf9
boma <boma.obomanu86@gmail.com>
2022-12-01 15:14:06 +0100

    Full Demo Test App

--- screens/BioData.js
+++ screens/BioData.js
@@ -1,58 +1,59 @@
-import React from 'react';
-import { Button, StyleSheet, Text, TextInput, View, Image, TouchableOpacity ,ScrollView} from 'react-native';
-import BioDataForm from './BioDataForm';
+import React from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import BioDataForm from "./BioDataForm";
 
-
-
-export default function BioData({navigation}){
-    return(
-        <View style =  {styles.container}>
-            <ScrollView style={styles.scrollView}>
-            <View style={styles.header}>
-            <Image source={ require("../assets/logo.png")} style={styles.image}/>
-                <Text>  Bio - Data Form</Text> 
-               
-            </View>
-    
-            <View style={styles.body}>
-                <Text style={styles.text}>Welcome to BioData</Text>
-                <BioDataForm navigationValue={navigation}/>
-            </View>
-            </ScrollView>
-    
+export default function BioData({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.header}>
+          <Image source={require("../assets/logo.png")} style={styles.image} />
+          <Text> Bio - Data Form</Text>
         </View>
-        )
-    }
-    
-    const styles = StyleSheet.create({
-        container: {
-          flex: 1,
-          backgroundColor: '#FAE2E2',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-        },
-    
-        header:{
-            padding:40,
-            flex:0.3,
-            alignSelf:'flex-start'
-    
-        },
-        body:{
-            flex:0.5,
-            alignItems: 'center',
-        },
-        text:{
-            fontWeight: 'bold',
-            fontSize: 20,
-        },
-        image:{
-            flexDirection: 'row',
-            height:80,
-            width:80,
-            borderRadius: 48,
-    
-            
-        }
-    });
-    
+
+        <View style={styles.body}>
+          <Text style={styles.text}>Welcome to BioData</Text>
+          <BioDataForm navigationValue={navigation} />
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FAE2E2",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+
+  header: {
+    padding: 40,
+    flex: 0.3,
+    alignSelf: "flex-start",
+  },
+  body: {
+    flex: 0.5,
+    alignItems: "center",
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  image: {
+    flexDirection: "row",
+    height: 80,
+    width: 80,
+    borderRadius: 48,
+  },
+});
